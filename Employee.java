23b60d3f5c8494808a930cb58b304d41e1f525a6
sayantani-das <79510331+sayantani-das@users.noreply.github.com>
2021-02-23 18:03:05 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,16 @@
+package collectionframework;
+
+public class Employee {
+	private String name;
+	private String dept;
+public Employee(String name,String dept)
+{
+	this.name=name;
+	this.dept=dept;
+}
+public String getname() {
+	return name;}
+public String getdept() {
+	return dept;}
+
+}
